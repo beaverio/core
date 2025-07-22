f95a122bfb05cad25f0427a5d52fde717d5b4db4
Connor Burge <connor.d.burge@gmail.com>
2025-07-22 18:34:46 -0400

    basic user regitstration in memory

--- src/main/java/com/beaver/core/config/WebSecurityConfig.java
+++ src/main/java/com/beaver/core/config/WebSecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +24,20 @@ public class WebSecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails connor = User.withUsername("connor")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        UserDetails kyndall = User.withUsername("kyndall")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(connor, kyndall);
     }
 }
