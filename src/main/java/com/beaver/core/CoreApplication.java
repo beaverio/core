6cba4aeb604a6f7342f4e62c360f38700eb7ccf8
Connor Burge <connor.d.burge@gmail.com>
2025-07-21 21:27:48 -0400

    initing project

--- src/main/java/com/beaver/core/CoreApplication.java
+++ src/main/java/com/beaver/core/CoreApplication.java
@@ -1,13 +1,22 @@
 package com.beaver.core;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory(".")
+                .ignoreIfMissing()
+                .load();
 
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(CoreApplication.class, args);
+    }
 }
