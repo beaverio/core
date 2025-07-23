6d263fb8e8b7c6902c029ff999f292ba24d4ee43
Connor Burge <connor.d.burge@gmail.com>
2025-07-22 20:58:26 -0400

    wip

--- src/main/java/com/beaver/core/config/WebSecurityConfig.java
+++ src/main/java/com/beaver/core/config/WebSecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,9 +21,24 @@ public class WebSecurityConfig {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable) // disables csrf token requirement
                 .authorizeHttpRequests(request -> request
-                                .requestMatchers("register", "login").permitAll()
+                                .requestMatchers("/register/**", "/login/**").permitAll()
                                 .anyRequest().authenticated()) // enforces authentication on all request
                 .httpBasic(Customizer.withDefaults()); // adds basic http authentication
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails connor = User.withUsername("connor")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        UserDetails kyndall = User.withUsername("kyndall")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(connor, kyndall);
     }
 }
