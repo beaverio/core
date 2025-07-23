c397a42537fd70e7b7875f10bc24ee420e9c3325
Connor Burge <connor.d.burge@gmail.com>
2025-07-22 21:23:38 -0400

    wip

--- src/main/java/com/beaver/core/repository/IUserRepository.java
+++ src/main/java/com/beaver/core/repository/IUserRepository.java
@@ -4,8 +4,10 @@ import com.beaver.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, UUID> {
     User findByEmail(String email);
-    User findByEmail(String email);
+}
