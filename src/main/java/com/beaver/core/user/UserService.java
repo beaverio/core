0a8ca5a4f1760df9f6ce8faadca1ac09a1305cdb
Connor Burge <connor.d.burge@gmail.com>
2025-07-25 19:18:03 -0400

    some caching work

--- src/main/java/com/beaver/core/user/UserService.java
+++ src/main/java/com/beaver/core/user/UserService.java
@@ -3,6 +3,9 @@ package com.beaver.core.user;
 import com.beaver.core.user.dto.UpdateSelf;
 import com.beaver.core.user.mapper.IUserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,29 +17,34 @@ public class UserService {
     private final IUserRepository userRepository;
     private final IUserMapper userMapper;
 
+    @Cacheable(value = "users", key = "#email")
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
+    @CachePut(value = "users", key = "#user.email")
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
+    @Cacheable(value = "users", key = "#email")
     public User getUserSelf(String email) {
-        return findByEmail(email)
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
+    @CachePut(value = "users", key = "#email")
     public User updateSelf(String email, UpdateSelf updateRequest) {
-        User existingUser = findByEmail(email)
+        User existingUser = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         userMapper.mapToEntity(updateRequest, existingUser);
         return userRepository.save(existingUser);
     }
 
+    @CacheEvict(value = "users", key = "#email")
     public void deleteUser(String email) {
-        User existingUser = findByEmail(email)
+        User existingUser = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         userRepository.delete(existingUser);
